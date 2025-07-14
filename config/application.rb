9b9f6241bc531b4ba9d88fdde697bd4ca85437fd
GrzegorzMika <g.w.mika@gmail.com>
2025-07-14 23:34:29 +0200

    fix

--- config/application.rb
+++ config/application.rb
@@ -1,22 +1,6 @@
 require_relative "boot"
 
-# require "rails/all"
-# Require the frameworks you want:
-require "active_model/railtie"
-# require "active_record/railtie" # We don't need this for a non-database app
-# require "active_storage/engine" # We don't need this
-require "action_controller/railtie"
-require "action_view/railtie"
-require "action_mailer/railtie"
-require "active_job/railtie"
-require "action_cable/engine"
-# require "action_mailbox/engine" # We don't need this
-# require "action_text/engine" # We don't need this
-require "rails/test_unit/railtie"
-# require "sprockets/railtie"
-# require "rails/health/railtie"
-require "turbo-rails" # If you are using Turbo
-# require "stimulus/railtie" # If you are using Stimulus
+require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
